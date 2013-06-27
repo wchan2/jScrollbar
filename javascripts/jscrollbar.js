32583490cb50346ada607c1f0a57508d9df7c0f8
will chan <willchan2@gmail.com>
2013-06-27 17:56:17 -0400

    refactor the jscrollbar to be its own object

--- javascripts/jscrollbar.js
+++ javascripts/jscrollbar.js
@@ -1,40 +1,65 @@
+// for vertical scrolling
+// http://stackoverflow.com/questions/3253199/remove-html-scrollbars-but-allow-mousewheel-scrolling
+
+// horizontal scrolling
+// http://stackoverflow.com/questions/2274627/how-can-i-get-horizontal-scrollbars-at-top-and-bottom-of-a-div
+
 ;(function( $ ) {
+
+  var jScrollbar = {
+    init: function(options) {
+      this.options        = $.extend({}, $.fn.jScrollbar.options, options);
+      this.$contentDiv    = $(this.options.contentDiv);
+      this.$scrollbar     = $('<div />'),
+      this.$scrollbarBody = $('<div />')
+      delete this.options.contentDiv;
+
+      this.setupDOM();
+      this.attachEvents();
+    },
+    setupDOM: function() {
+      this.$scrollbarBody.css({
+        width: this.$contentDiv[0].scrollWidth,
+        paddingTop: '1px'
+      });
+
+      this.$scrollbar.css({
+        width: this.$contentDiv.width() + 3,
+        overflowX: 'auto'
+      }).append(this.$scrollbarBody);
+
+      this.$contentDiv.before(this.$scrollbar);
+    },
+    attachEvents: function() {
+      var $contentDiv = this.$contentDiv,
+          $scrollbar = this.$scrollbar;
+
+      $scrollbar.on('scroll', function(event) {
+        $contentDiv.get(0).scrollLeft = this.scrollLeft;
+      });
+
+      $contentDiv.on('scroll', function(event) {
+        $scrollbar.get(0).scrollLeft = this.scrollLeft;
+      });
+    }
+  };
   
   $.fn.jScrollbar = function() {
     // placing it here so that its quicker to load
+    var selector = this.selector;
+
     $(this).css({
       paddingTop: '1px',
       overflowX: 'hidden'
     });
 
     return this.each(function() {
-      var $this = $(this),
-          $scrollbar = $('<div />'),
-          $scrollbarBody = $('<div />');
-
-      $scrollbarBody.css({
-        width: $this[0].scrollWidth,
-        paddingTop: '1px'
-      });
-
-      $scrollbar.css({
-        width: $this.width() + 3,
-        overflowX: 'auto'
-      }).append($scrollbarBody);
-
-      $scrollbar.on('scroll', function(event) {
-        $this.get(0).scrollLeft = this.scrollLeft;
-      });
-
-      $this.on('scroll', function(event) {
-        $scrollbar.get(0).scrollLeft = this.scrollLeft;
-      });
-
-      $this.before($scrollbar);
+      var scrollview = Object.create(jScrollbar);
+      scrollview.init({ contentDiv: selector });
     });
   };
 
+  // initializing the options
   $.fn.jScrollbar.options = {};
-
 
 }( jQuery ));
